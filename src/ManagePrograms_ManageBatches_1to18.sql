aeab89fc4b857e5696e8aebc07d0117c16370a57
jayashree216 <jayashreesatapathy2009@gmail.com>
2022-04-09 18:22:39 -0400

    Update ManagePrograms_ManageBatches_1to18.sql

--- src/ManagePrograms_ManageBatches_1to18.sql
+++ src/ManagePrograms_ManageBatches_1to18.sql
@@ -1,1 +1,6 @@
--- 1 - Create a new program
+QUERY 1:
+Create a new program
+
+insert into tbl_lms_program (program_id, program_name, program_description, program_status) values (7865, 'create', 'test_desc', 'active');
+
+
