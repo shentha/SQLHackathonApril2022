7e91f20d82e2ff513a4ac71dd7d0cac9fffa0c3d
shentha <96349865+shentha@users.noreply.github.com>
2022-04-10 18:41:07 -0400

    Update Selvi.sql

--- src/Selvi.sql
+++ src/Selvi.sql
@@ -1,9 +1,13 @@
 /* 9. Get the program with maximum number of users */
 WITH rslt1 AS 
-  ( SELECT  pgm.program_name pgmname, COUNT(pgm.program_name) usrcount 
+  ( SELECT  pgm.program_name pgmname, 
+            COUNT(pgm.program_name) usrcount 
     FROM tbl_lms_program pgm 
     JOIN tbl_lms_batch batch ON pgm.program_id = batch.batch_program_id 
     JOIN tbl_lms_userbatch_map ubm  ON batch.batch_id = ubm.batch_id 
+	JOIN tbl_lms_userrole_map urm ON ubm.user_role_id = urm.user_role_id
+	JOIN tbl_lms_role rle ON urm.role_id = rle.role_id
+	WHERE rle.role_name = 'Student'
 	GROUP BY pgm.program_name 
   ) 
 	
@@ -27,11 +31,11 @@ WHERE batch.batch_program_id = ( SELECT pgm.program_id
 
  SELECT * FROM batches_program_view		
  
- /* 32 - Create new User Profile */
+ /* 32 - Get user info for users based on the following filters: 1) filter by first or last name 2) filter by location 3) filter by timezone. Also find out how many users are  there for each timezone" */ 
 	SELECT usr.user_time_zone time_zone , 
 	       COUNT( usr.user_time_zone)
     FROM tbl_lms_user usr
-    WHERE ( LOWER(usr.user_first_name) LIKE LOWER('suh%') 
+    WHERE ( LOWER(usr.user_first_name) LIKE LOWER('Robert%') 
           OR LOWER(usr.user_last_name) LIKE LOWER('Louis%') )
 	      OR user_location LIKE 'Atlanta'
 	      OR user_time_zone LIKE 'IST' 
@@ -64,3 +68,43 @@ SELECT login.user_security_q, login.user_security_a
 FROM tbl_lms_user_login login 
 WHERE user_login_name = 'Sarah.Anderson@gmail.com'
 
+
+/* 90 - Create a table named tbl_lms_hackathon */ 
+CREATE TABLE public.tbl_lms_hackathon (
+    hackathon_id integer NOT NULL PRIMARY KEY,
+    hackathon_name varchar(20) NOT NULL,
+    hackathon_description varchar,
+    start_date date NOT NULL DEFAULT CURRENT_DATE ,
+    end_date date NOT NULL
+  );
+commit;
+
+/* sequence generation */
+CREATE SEQUENCE public.tbl_lms_hackathon_hackathon_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER TABLE public.tbl_lms_hackathon_hackathon_id_seq OWNER TO postgres;
+
+
+/* 91 - Create a table named tbl_lms_user_hackathon_map */ 
+	CREATE TABLE public.tbl_lms_user_hackathon_map (
+       user_hackathon_id integer NOT NULL PRIMARY KEY,
+       user_id character varying NOT NULL,
+       hackathon_id integer NOT NULL,
+       FOREIGN KEY (user_id) REFERENCES tbl_lms_user(user_id) ON DELETE CASCADE,
+       FOREIGN KEY (hackathon_id) REFERENCES tbl_lms_hackathon(hackathon_id) ON DELETE CASCADE,
+       UNIQUE (user_id, hackathon_id)
+    );
+    commit;
+  
+  /* sequence generation */
+CREATE SEQUENCE public.tbl_lms_user_hackathon_map_user_hackathon_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER TABLE public.tbl_lms_user_hackathon_map_user_hackathon_id_seq OWNER TO postgres;
