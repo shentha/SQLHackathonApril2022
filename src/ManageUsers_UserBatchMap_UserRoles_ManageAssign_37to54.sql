f729cd146869527c7fa60f2f1dbc0b47a6c74f34
raji032011 <rajalakshmi.kr@gmail.com>
2022-04-11 13:44:50 -0400

    Update ManageUsers_UserBatchMap_UserRoles_ManageAssign_37to54.sql

--- src/ManageUsers_UserBatchMap_UserRoles_ManageAssign_37to54.sql
+++ src/ManageUsers_UserBatchMap_UserRoles_ManageAssign_37to54.sql
@@ -1,6 +1,1 @@
 -- 37 - Update user login information 1) update security question and answer 2) update login status
-
-update tbl_lms_user_login
-set user_security_q= 'which is your favortite dish', user_security_a ='Pizza',
-user_login_status='INACTIVE'
-where user_id='U01'
