2e636caa7d5327d804e91c20506c1d3e869cdd8c
raji032011 <101362543+raji032011@users.noreply.github.com>
2022-04-11 14:00:28 -0400

    someone else committed

--- src/ManageUsers_UserBatchMap_UserRoles_ManageAssign_37to54.sql
+++ src/ManageUsers_UserBatchMap_UserRoles_ManageAssign_37to54.sql
@@ -10,3 +10,13 @@ where user_id='U01'
 
 -- Verifying output:
 select * from tbl_lms_user_login
+
+-- QUERY 38:
+Get user user id, firstname, last name, login name, password and user firstname in ascending order.
+
+select tbl_lms_user.user_id as uid,tbl_lms_user.user_first_name,tbl_lms_user.user_last_name,
+tbl_lms_user_login.user_login_name,tbl_lms_user_login.user_password 
+from tbl_lms_user,
+tbl_lms_user_login
+order by uid ASC
+
